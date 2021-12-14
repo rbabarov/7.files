d12a1a50676c8035f653f373a96be90e201b3d20
rbabarov <90253870+rbabarov@users.noreply.github.com>
2021-12-14 11:02:02 +0500

    create cook book modifited

--- main.py
+++ main.py
@@ -2,28 +2,43 @@ def create_cook_book(file):
     cook_book = {}
     name_dish = ''
     list_indigrients = []
-    str_indigrient = {}
+    #str_indigrient = {}
 
-    doc_file = open(file, 'r', encoding="utf-8")
-    lines = doc_file.readlines()
+    with open(file, 'r', encoding="utf-8") as doc_file:
 
-    for line in lines:
-        line = line.replace('\n', '')
-        splitarray = line.split('|')
+        for line in doc_file:
+            print(line)
+            line = line.strip()
+            if line != '' and not line.isdigit():
+                name_dish = line
+            elif line.isdigit():
+                count = int(line)
+                #раз в этой строке количество индигриетов в блюде,
+                # значит дальше будет Х строк с индигриентами
+                for x in range(count):
 
-        if line != '' and len(splitarray) < 2 and not line.isdigit():
-            print(line, ' Обнаруженно название')
-            name_dish = line
-        elif line.isdigit():
-            print(line, ' в строке одни цифры')
-        elif len(splitarray) > 2:
-            print(line, ' в этой строке индигриенты')
-            for str_ in splitarray:
+                    lstind = doc_file.readline().strip()
+                    splitarray = lstind.split('|')
+                    #print(splitarray)
+                    str_indigrient = {}
+                    str_indigrient['ingredient_name'] = splitarray[0]
+                    str_indigrient['quantity'] = int(splitarray[1])
+                    str_indigrient['measure'] = splitarray[2]
+                    list_indigrients += [str_indigrient]
+                #print(list_indigrients)
 
-            str_indigrient
+                try:
+                    cook_book[name_dish] += list_indigrients
+                except:
+                    cook_book[name_dish] = list_indigrients
+
+    return cook_book
+
+
 
 
 
 
 cook_book = create_cook_book('recipes.txt')
+print(cook_book)
 

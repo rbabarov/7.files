4335f1fcd11f6bbe5b71598628cb2413bef7eaab
rbabarov <90253870+rbabarov@users.noreply.github.com>
2021-12-14 14:29:11 +0500

    create cook book fixed

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
 def create_cook_book(file):
     cook_book = {}
-    name_dish = ''
-    list_indigrients = []
-    #str_indigrient = {}
-
+    str_indigrient = {}
     with open(file, 'r', encoding="utf-8") as doc_file:
 
         for line in doc_file:
-            print(line)
             line = line.strip()
             if line != '' and not line.isdigit():
                 name_dish = line
+                cook_book[name_dish] = []
             elif line.isdigit():
                 count = int(line)
+                list_indigrients = []
                 #раз в этой строке количество индигриетов в блюде,
                 # значит дальше будет Х строк с индигриентами
                 for x in range(count):
 
                     lstind = doc_file.readline().strip()
                     splitarray = lstind.split('|')
-                    #print(splitarray)
                     str_indigrient = {}
-                    str_indigrient['ingredient_name'] = splitarray[0]
+                    str_indigrient['ingredient_name'] = splitarray[0].strip()
                     str_indigrient['quantity'] = int(splitarray[1])
-                    str_indigrient['measure'] = splitarray[2]
+                    str_indigrient['measure'] = splitarray[2].strip()
+                    splitarray = []
                     list_indigrients += [str_indigrient]
-                #print(list_indigrients)
 
-                try:
-                    cook_book[name_dish] += list_indigrients
-                except:
-                    cook_book[name_dish] = list_indigrients
+                cook_book[name_dish] += list_indigrients
+
 
     return cook_book
 
